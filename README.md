6647590fd4a4e72734ef8e1da45aed62e7b59f03
hdy5049 <hdy5049@naver.com>
2024-10-29 11:23:21 +0900

    googleVisionAPI3

--- README.md
+++ README.md
@@ -1,5 +1,37 @@
 # project1-2024
 2024-2학기 캡스톤프로젝트 수업
+openAPI를 사용한 인공지능 시스템 실습
+# openweathermap
+
+지정된 장소의 현재 날씨를 표시
+https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35
+[실습해보기](https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35)
+```
+$.ajax({
+          type: "GET",
+          url: 'https://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=7d96bc5108f52b80e2d9075a369b9f35',
+       }).done(function(response) {
+
+             console.log(response)
+            //  alert(response.coord.lat)
+
+             let wdata = response
+             let exdata = response.weather[0];
+         
+             temp.innerText = wdata.main.temp + "°C";
+             min.innerText = wdata.main.temp_min;
+             max.innerText = wdata.main.temp_max;
+             wind.innerText = wdata.wind.speed;
+         
+             weather.innerText = exdata.main + "," + exdata.description;
+             icon.setAttribute('src', icon_url + exdata.icon + ".png");
+       }).fail(function(error) {
+          alert("!/js/user.js에서 에러발생: " + error.statusText);
+       });
+       ```
+# openAI
+
+# google cloud vision
 
 개발순서
 1. 소스수정
--- googleVisionAPI/vision.js
+++ googleVisionAPI/vision.js
@@ -1,48 +1,81 @@
-GOOGLEAPI_KEY = ""
-function processFile(event){
-    content = event.target.result
-    imagestring = content.replace('data:image/jpeg;base64,','')
-    document.getElementById("gimage").src = content
+GOOGLEAPI_KEY = "AIzaSyCzGLuB8l2aqu_kwUyvsRFg3_HzTNnMvEw"
+CV_URL = 'https://vision.googleapis.com/v1/images:annotate?key=' + GOOGLEAPI_KEY;
+function processFile(event) {
+    const content = event.target.result;
+    imagestring = content.replace(/^data:image\/(jpeg|png);base64,/, '');  // JPEG 또는 PNG 형식의 이미지를 처리
+    document.getElementById("gimage").src = content;  // 이미지 미리보기 표시
 }
 
-
-function uploadFiles(files){
-    file = files[0]
-    reader = new FileReader()
-    reader.onloadend = processFile
-    reader.readAsDataURL(file)
+function uploadFiles(files) {
+    const file = files[0];
+    const reader = new FileReader();
+    reader.onloadend = processFile;
+    reader.readAsDataURL(file);  // 파일을 Base64로 인코딩
 }
 
-function analyze(){
-    data ={
+function analyze() {
+    if (!imagestring) {
+        alert("먼저 이미지를 업로드하세요.");
+        return;
+    }
+
+    const data = {
         requests: [{
-            image:{
-                content: imagestirng
+            image: {
+                content: imagestring  // Base64로 인코딩된 이미지 데이터
             },
-            features:[{
-                type:"FACE__DETECTION",
-                maxResults:100
+            features: [{
+                type: "FACE_DETECTION",  // 얼굴 감지 기능
+                maxResults: 100
             }]
         }]
-            }
-    
+    };
+
 
     $.ajax({
-        type:"POST",
-        url: "POST https://vision.googleapis.com/v1/images:annotate" + "?key="+ GOOGLEAPI_KEY,
-        headers:{
-            "Accept" : "application/json",
-            "Content-Type" : "application/json"
+        type: "POST",
+        url: 'https://vision.googleapis.com/v1/images:annotate?key=' + GOOGLEAPI_KEY,  // API 키를 사용하여 요청
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
         },
         data: JSON.stringify(data),
         contentType: "application/json; charset=utf-8"
-    }).done( function(response){
-        console.log(response)
-        
-    }).fail(function(error){
-        console.log(error)
-       
-    }
+    }).done(function (response) {
+        const faces = response.responses[0].faceAnnotations;  // 얼굴 인식 결과
+        if (faces && faces.length > 0) {
+            let resultText = `총 ${faces.length}개의 얼굴이 감지되었습니다:\n\n`;
+            faces.forEach((face, index) => {
+                resultText += `얼굴 #${index + 1}:\n`;
+                resultText += `  감정 표현: ${detectEmotions(face)}\n`;
+                resultText += `  기울기 (좌우): ${face.tiltAngle}\n`;
+                resultText += `  기울기 (위아래): ${face.panAngle}\n`;
+                resultText += `  기울기 (좌우 회전): ${face.rollAngle}\n\n`;
+            });
+            $("#result").val(resultText);  // 결과를 textarea에 표시
+        } else {
+            $("#result").val("얼굴을 감지하지 못했습니다.");
+        }
+    }).fail(function (error) {
+        console.error(error);
+        $("#result").val("얼굴 인식 중 오류가 발생했습니다.");
+    });
+}
 
-    )
+function detectEmotions(face) {
+    const likelihoods = {
+        VERY_UNLIKELY: "거의 없음",
+        UNLIKELY: "적음",
+        POSSIBLE: "가능성 있음",
+        LIKELY: "높음",
+        VERY_LIKELY: "매우 높음"
+    };
+
+    let emotions = '';
+    emotions += `웃음: ${likelihoods[face.joyLikelihood]}\n`;
+    emotions += `슬픔: ${likelihoods[face.sorrowLikelihood]}\n`;
+    emotions += `화남: ${likelihoods[face.angerLikelihood]}\n`;
+    emotions += `놀람: ${likelihoods[face.surpriseLikelihood]}\n`;
+
+    return emotions;
 }
